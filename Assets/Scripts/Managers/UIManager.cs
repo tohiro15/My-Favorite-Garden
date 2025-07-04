2c1faba89f594c1206aaa0746d0e4738b0bcde53
Daniil <136441101+tohiro15@users.noreply.github.com>
2025-07-04 18:03:43 +0800

    feat: basic HUD and gardenbed prefabs
    Created basic HUD (no functionality yet):
    
    - Inventory bar at the bottom
    - Backpack button in bottom-right corner
    - Pressing I or clicking the button will open inventory (logic to be added)
    - Money counter at top-left
    
    - Refactored gardenbeds into separate prefabs for easier reuse and placement

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -3,16 +3,41 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Shop")]
+    [Space]
+
     [SerializeField] private Canvas _shopCanvas;
     [SerializeField] private Button _exitButton;
+
+    [Header("Backpack")]
+    [Space]
+
+    [SerializeField] private GameObject _backpackPanel;
+    [SerializeField] private Button _backpackButton;
+    private bool _openBackpack = false;
 
     private void Start()
     {
         _exitButton?.onClick.RemoveAllListeners();
         _exitButton?.onClick.AddListener(CloseShopCanvas);
+
+        _backpackButton?.onClick.RemoveAllListeners();
+        _backpackButton?.onClick.AddListener(OpenCloseBackpackPanel);
+        _backpackPanel?.gameObject.SetActive(_openBackpack);
+
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I)) OpenCloseBackpackPanel();
     }
     private void CloseShopCanvas()
     {
         _shopCanvas?.gameObject.SetActive(false);
+    }
+
+    private void OpenCloseBackpackPanel()
+    {
+        _openBackpack = !_openBackpack;
+        _backpackPanel?.gameObject.SetActive(_openBackpack);
     }
 }
