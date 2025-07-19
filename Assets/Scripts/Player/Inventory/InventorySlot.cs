f41984f3074970abc6ba435897229d46992a9f64
Daniil <136441101+tohiro15@users.noreply.github.com>
2025-07-20 03:11:54 +0800

    feat: implemented inventory slot selection system with animation
    - Added inventory slot selection system
    - Selected slot highlights in yellow and scales up (using DOTween)
    - TODO: implement item appearance in hand after selection
    - Logically separated backpack and inventory slots

--- Assets/Scripts/Player/Inventory/InventorySlot.cs
+++ Assets/Scripts/Player/Inventory/InventorySlot.cs
@@ -1,25 +1,75 @@
+using DG.Tweening;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
+    [SerializeField] private Image _background;
+
+    private Color _normalColor = Color.white;
+    private Color _selectedColor = Color.yellow;
+
+    private float _normalScale = 1f;
+    private float _selectedScale = 1.2f;
+    private float _tweenDuration = 0.15f;
+
+    private InventoryUI _inventoryUI;
     private Item _item;
     private GameObject _itemPrefab;
     private int _slotIndex;
     private bool _isEmpty = true;
     public Item Item => _item;
     public bool IsEmpty => _isEmpty;
+    private bool _isSelectable = true;
+    private bool _isSelected = false;
 
     private void Awake()
     {
         _item = GetComponentInChildren<Item>();
         if(_item != null ) _isEmpty = false; 
     }
-    public void Initialization(GameObject itemPrefab, int index)
+
+    public void Initialization(GameObject itemPrefab, int index, InventoryUI parentUI, Color normalColor, Color selectedColor, float normalScale, float selectedScale, float tweenDuration, bool isSelectable = true)
     {
         _itemPrefab = itemPrefab;
         _slotIndex = index;
+        _inventoryUI = parentUI;
+        _isSelectable = isSelectable;
+
+        _normalColor = normalColor;
+        _selectedColor = selectedColor;
+
+        _normalScale = normalScale;
+        _selectedScale = selectedScale;
+        _tweenDuration = tweenDuration;
+
+        _background.color = _normalColor;
+        transform.localScale = Vector3.one * _normalScale;
     }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!_isSelectable) return;
+        if (_isSelected) return;
+        _inventoryUI.SelectSlot(_slotIndex);
+    }
+
+    public void Select()
+    {
+        _isSelected = true;
+        _background.DOColor(_selectedColor, _tweenDuration);
+        transform.DOScale(Vector3.one * _selectedScale, _tweenDuration);
+    }
+
+    public void Deselect()
+    {
+        _isSelected = false;
+        _background.DOColor(_normalColor, _tweenDuration);
+        transform.DOScale(Vector3.one * _normalScale, _tweenDuration);
+    }
+
     public void Add(ItemData itemData, int count)
     {
         if (_item == null)
--- Assets/Scripts/Player/Inventory/InventoryUI.cs
+++ Assets/Scripts/Player/Inventory/InventoryUI.cs
@@ -3,32 +3,55 @@ using UnityEngine;
 public class InventoryUI : MonoBehaviour
 {
     [SerializeField] private GameObject _itemPrefab;
-    [Space]
-    [SerializeField] private InventorySlot[] _inventorySlots;
-    [SerializeField] private ItemData[] _allItemTypes;
 
+    [Space]
+    [Header("Slot Settings")]
+    [Space]
+
+    [SerializeField] private InventorySlot[] _inventorySlots;
+    [SerializeField] private InventorySlot[] _backpackSlots;
+    [SerializeField] private ItemData[] _allItemTypes;
+    [Header("Color Settigs")]
+    [SerializeField] private Color _normalColor = Color.white;
+    [SerializeField] private Color _selectedColor = Color.yellow;
+    [SerializeField] private Color _backpackColor;
+    [Header("Other Settings")]
+    [SerializeField] private float _normalSlotScale = 1f;
+    [SerializeField] private float _selectedSlotScale = 1.2f;
+    [SerializeField] private float _tweenSlotDuration = 0.15f;
     public InventorySlot[] InventorySlots => _inventorySlots;
+    public InventorySlot[] BackpackSlots => _backpackSlots;
+
+    private int _selectedSlot = -1;
+
     private void Awake()
     {
-        for(int i = 0;  i < _inventorySlots.Length; i++)
-        {
-            _inventorySlots[i].Initialization(_itemPrefab,i);
-        }
+        for (int i = 0; i < _inventorySlots.Length; i++)
+            _inventorySlots[i].Initialization(_itemPrefab, i, this, _normalColor, _selectedColor, _normalSlotScale, _selectedSlotScale, _tweenSlotDuration, true);
+
+        for (int i = 0; i < _backpackSlots.Length; i++)
+            _backpackSlots[i].Initialization(_itemPrefab, i, this, _backpackColor, _backpackColor, _normalSlotScale, _selectedSlotScale, _tweenSlotDuration, false);
+
         foreach (var itemData in _allItemTypes)
         {
             string keySlot = $"SlotFor_{itemData.name}";
             string keyCount = $"CountFor_{itemData.name}";
-
             int savedSlot = PlayerPrefs.GetInt(keySlot, -1);
             int savedCount = PlayerPrefs.GetInt(keyCount, itemData.DefaultCount);
 
             if (savedSlot >= 0 && savedSlot < _inventorySlots.Length && savedCount > 0)
-            {
                 _inventorySlots[savedSlot].Add(itemData, savedCount);
-            }
+            else if (savedSlot >= 0 && savedSlot < _backpackSlots.Length && savedCount > 0)
+                _backpackSlots[savedSlot].Add(itemData, savedCount);
         }
     }
 
+    private void Update()
+    {
+        for (int i = 0; i < _inventorySlots.Length; i++)
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                SelectSlot(i);
+    }
 
     public void AddItem(ItemData itemData, int count)
     {
@@ -52,4 +75,19 @@ public class InventoryUI : MonoBehaviour
             }
         }
     }
+
+    public void SelectSlot(int index)
+    {
+        if (index == _selectedSlot) return;
+
+        if (_selectedSlot >= 0)
+            _inventorySlots[_selectedSlot].Deselect();
+        _selectedSlot = index;
+        _inventorySlots[_selectedSlot].Select();
+
+        // TODO: здесь можно инициировать появление предмета в руке
+        // var selectedItem = _inventorySlots[_selectedSlot].Item;
+        // HandController.Instance.Hold(selectedItem);
+    }
+
 }
