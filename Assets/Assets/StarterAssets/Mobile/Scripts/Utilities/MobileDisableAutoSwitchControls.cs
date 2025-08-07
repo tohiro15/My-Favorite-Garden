40686a2b2dd38082b68f5740431c079f6b618c13
Daniil <136441101+tohiro15@users.noreply.github.com>
2025-08-07 16:29:36 +0800

    fix: improved device check for enabling mobile UI
    - Refactored logic to detect if the device is a mobile phone
    - Improved accuracy of showing mobile joystick UI
    - Now mobile UI appears only on mobile devices

--- Assets/Assets/StarterAssets/Mobile/Scripts/Utilities/MobileDisableAutoSwitchControls.cs
+++ Assets/Assets/StarterAssets/Mobile/Scripts/Utilities/MobileDisableAutoSwitchControls.cs
@@ -6,11 +6,7 @@ public class MobileDisableAutoSwitchControls : MonoBehaviour
     [SerializeField] private Canvas _mobileJoysticksCanvas;
     private void Start()
     {
-        _mobileJoysticksCanvas = GetComponent<Canvas>();
-        if (_mobileJoysticksCanvas != null)
-        {
-            if (GP_Device.IsMobile()) _mobileJoysticksCanvas.gameObject.SetActive(true);
-            else _mobileJoysticksCanvas.gameObject.SetActive(false);
-        }
+        if (GP_Device.IsMobile()) _mobileJoysticksCanvas.gameObject.SetActive(true);
+        else _mobileJoysticksCanvas.gameObject.SetActive(false);
     }
 }
